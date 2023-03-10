65c5a587ecfb4bf60f93b5ae39972bacf553f867
ElainaSann <mikihinasann@gmail.com>
2023-03-11 00:15:57 +0700

    PUSH!!!

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token: "TOKEN",
+    nodes: [
+        {
+            host: "localhost",
+            password: "youshallnotpass",
+            port: 2333,
+        }
+    ]
+}
